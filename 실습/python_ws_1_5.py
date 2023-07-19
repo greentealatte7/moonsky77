7b520ad692d911d37787da791e174f3c08a4aa44
unknown <moonsky37073@gmail.com>
2023-07-19 16:25:00 +0900

    실습폴더올림

---
+++ 실습/python_ws_1_5.py
@@ -0,0 +1,15 @@
+print(-3*2)
+print(3**2)
+print((3 ** 2) // (3*2))
+print((3 ** 2) % (3*2)) 
+print((3 ** 2)+((-3) ** 2))
+
+
+T = 3
+W = 2
+O = -3
+print(O*W)
+print(T**W)
+print((T ** W) // (T * W))
+print((T ** W) % (T * W)) 
+print((T ** W)+(O ** W))
